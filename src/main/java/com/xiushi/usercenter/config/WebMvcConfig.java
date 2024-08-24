2890965a2c251d60e83b5145ebff30e0ee965ca6
Riaduhb <1501303212@qq.com>
2024-08-24 18:27:24 +0800

    部署配置

---
+++ src/main/java/com/xiushi/usercenter/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.xiushi.usercenter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://124.221.4.74")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
+
